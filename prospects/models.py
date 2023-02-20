e831b2a3eb4415e86c244890de58039ebec522ae
Derrick Ampire <ampire90@gmail.com>
2023-02-20 12:49:30 +0300

    Prospect class created

--- prospects/models.py
+++ prospects/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Prospect(models.Model):
+     first_name = models.CharField(max_length=20)
+     last_name = models.CharField(max_length=20)
+     age = models.IntegerField(default=0)
+     email = models.EmailField()
