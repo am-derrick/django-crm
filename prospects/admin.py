c306bd8912895d945b4a35d057cf457e4eee5a0f
Derrick Ampire <ampire90@gmail.com>
2023-03-01 19:23:31 +0300

    admin models added

--- prospects/admin.py
+++ prospects/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, SalesRep, Prospect
+
+admin.site.register(User)
+admin.site.register(SalesRep)
+admin.site.register(Prospect)
