fe467528bf93f9d557675069c0f6ffc979ef263c
Derrick Ampire <ampire90@gmail.com>
2023-03-01 20:58:08 +0300

    list and detail functions added

--- prospects/views.py
+++ prospects/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Prospect
 
 
-def home(request):
-    return render(request, "home.html")
+def prospect_list(request):
+    prospects = Prospect.objects.all()
+    context = {
+        "prospects": prospects
+    }
+    return render(request, "prospects_list.html", context)
+
+def prospect_detail(request, pk):
+    prospect = Prospect.objects.get(id=pk)
+    context = {
+        "prospects": prospect
+    }
+    return render(request, "prospects_detail.html", context)
